68a6901f716370b7f1f4a24efdafdd377fad6523
Shahad161 <shahad.hamza.014@gmail.com>
2021-07-09 10:52:15 +0300

    add RXjava to basic calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,178 +1,51 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.example.calculator.databinding.ActivityMainBinding
-import java.lang.Math.cbrt
-import kotlin.math.*
-
+import io.reactivex.rxjava3.core.Observable
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    var secondNumber : Double = 0.0
-    var factorial : Double = 1.0
-    var lastNumber : Double = 0.0
-    var currentOperation: Operation? = null
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        RX()
         addCallbacks()
-
     }
     private fun addCallbacks() {
+        binding.appCompatButton18.setOnClickListener {
+            binding.numbers.text = addToInputText(".")
+        }
+        binding.Div.setOnClickListener {
+            binding.numbers.text = addToInputText("÷")
+        }
+        binding.Multiplication.setOnClickListener {
+            binding.numbers.text = addToInputText("×")
+        }
+        binding.Minus.setOnClickListener {
+            binding.numbers.text = addToInputText("-")
+        }
+        binding.plus.setOnClickListener {
+            binding.numbers.text = addToInputText("+")
+        }
         binding.clear.setOnClickListener {
             clearInput()
         }
-        binding.plus.setOnClickListener {
-            prepareOPeration(Operation.Plus)
-        }
-        binding.Minus.setOnClickListener {
-            prepareOPeration(Operation.Minus)
-        }
-        binding.Div.setOnClickListener {
-            prepareOPeration(Operation.Div)
-        }
-        binding.Multiplication.setOnClickListener {
-            prepareOPeration(Operation.Multiplication)
-        }
-        binding.percentage.setOnClickListener {
-            prepareOPeration(Operation.Percentage)
-        }
-        binding.sin.setOnClickListener {
-            prepareOPeration(Operation.Sin)
-        }
-        binding.cos.setOnClickListener {
-            prepareOPeration(Operation.Cos)
-        }
-        binding.tan.setOnClickListener {
-            prepareOPeration(Operation.Tan)
-        }
-        binding.Log2.setOnClickListener {
-            prepareOPeration(Operation.Log2)
-        }
-        binding.Log10.setOnClickListener {
-            prepareOPeration(Operation.Log10)
-        }
-        binding.ln.setOnClickListener {
-            prepareOPeration(Operation.Ln)
-        }
-        binding.exp.setOnClickListener {
-            prepareOPeration(Operation.Exp)
-        }
-        binding.X2.setOnClickListener {
-            prepareOPeration(Operation.X2)
-        }
-
-        binding.Xn.setOnClickListener {
-            prepareOPeration(Operation.Xn)
-        }
-        binding.squareRoot.setOnClickListener {
-            prepareOPeration(Operation.SquareRoot)
-        }
-        binding.cubeRoot.setOnClickListener {
-            prepareOPeration(Operation.CubeRoot)
-        }
-        binding.sinh.setOnClickListener {
-            prepareOPeration(Operation.Sinh)
-        }
-        binding.cosh.setOnClickListener {
-            prepareOPeration(Operation.Cosh)
-        }
-        binding.tanh.setOnClickListener {
-            prepareOPeration(Operation.Tanh)
-        }
-        binding.mod.setOnClickListener {
-            prepareOPeration(Operation.Mod)
-        }
-        binding.factorial.setOnClickListener {
-            prepareOPeration(Operation.Factorial)
-        }
-        binding.xx.setOnClickListener {
-            prepareOPeration(Operation.XX)
-        }
-        binding.Result.setOnClickListener {
-            val result = doCurrentOpertion()
-            binding.numbers.text = result.toString()
-        }
-    }
-    private fun doCurrentOpertion(): Double {
-        return when(currentOperation){
-            Operation.Plus -> {
-                secondNumber()
-                lastNumber + secondNumber
-            }
-            Operation.Minus -> {
-                secondNumber()
-                lastNumber - secondNumber
-            }
-            Operation.Div -> {
-                secondNumber()
-                lastNumber / secondNumber
-            }
-            Operation.Multiplication -> {
-                secondNumber()
-                lastNumber * secondNumber
-            }
-            Operation.Percentage -> {
-                secondNumber()
-                lastNumber / 100 * secondNumber
-            }
-            Operation.X2 -> lastNumber * lastNumber
-            Operation.Xn -> {
-                secondNumber()
-                lastNumber.pow(secondNumber)
-            }
-            Operation.XX -> 1 / lastNumber
-            Operation.Sin -> sin(lastNumber)
-            Operation.Cos -> cos(lastNumber)
-            Operation.Tan -> tan(lastNumber)
-            Operation.Log2 -> log2(lastNumber)
-            Operation.Log10 -> log10(lastNumber)
-            Operation.Ln -> ln(lastNumber)
-            Operation.Exp -> exp(lastNumber)
-            Operation.Mod -> {
-                secondNumber()
-                lastNumber.rem(secondNumber)
-            }
-            Operation.Sinh -> sinh(lastNumber)
-            Operation.Cosh -> cosh(lastNumber)
-            Operation.Tanh -> tanh(lastNumber)
-            Operation.SquareRoot -> sqrt(lastNumber)
-            Operation.CubeRoot -> cbrt(lastNumber)
-            Operation.Factorial -> {
-                factorial()
-                factorial
-            }
-
-            else -> 0.0
-        }
-    }
-    fun factorial(){
-        var number = lastNumber
-        for(i in 1.. number.toInt()){
-            factorial*= i
-        }
-
-    }
-
-    private fun prepareOPeration(operation: Operation) {
-        lastNumber = binding.numbers.text.toString().toDouble()
-        clearInput()
-        currentOperation = operation
     }
 
     private fun clearInput() {
         binding.numbers.text = ""
     }
-
-    private fun secondNumber(){
-        secondNumber = binding.numbers.text.toString().toDouble()
-    }
-
 
     fun onClickNumber(v: View){
         val newDigit = (v as Button).text.toString()
@@ -180,10 +53,41 @@ class MainActivity : AppCompatActivity() {
         val newTextNumber = oldDigit + newDigit
         binding.numbers.text = newTextNumber
     }
-    fun Delete(v: View){
-        var text = binding.numbers.text
-        binding.numbers.text = text.substring(0, text.length-1)
+
+    private fun addToInputText(buttonValue: String): String {
+        return "${binding.numbers.text}$buttonValue"
     }
 
+    private fun getInputExpression(): String {
+        var expression = binding.numbers.text.replace(Regex("÷"), "/")
+        expression = expression.replace(Regex("×"), "*")
+        return expression
+    }
+
+    private fun showResult() {
+        val expression = getInputExpression()
+        val result = Expression(expression).calculate()
+        if (result.isNaN()) {
+            binding.operation.text = "Error"
+        } else {
+            binding.operation.text = DecimalFormat("0.######").format(result).toString()
+        }
+    }
+
+    fun RX(){
+        val observable = Observable.create<String>{ emitter ->
+            binding.numbers.doOnTextChanged { text, start, before, count ->
+                emitter.onNext(text.toString())
+            }
+        }.debounce(1, TimeUnit.SECONDS)
+        observable.subscribe(
+            { t ->
+                showResult()
+            },
+            { e ->
+                Log.i("TAG", "ON error")
+            }
+        )
+    }
 }
 
