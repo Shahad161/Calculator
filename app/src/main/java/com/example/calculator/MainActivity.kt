75f5d3968f490a844c704c9195efc0b987d38954
Shahad161 <shahad.hamza.014@gmail.com>
2021-07-10 16:30:01 +0300

    remove buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import com.example.calculator.databinding.ActivityMainBinding
@@ -14,66 +12,20 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         RX()
-        addCallbacks()
     }
-    private fun addCallbacks() {
-        binding.appCompatButton18.setOnClickListener {
-            binding.numbers.text = addToInputText(".")
-        }
-        binding.Div.setOnClickListener {
-            binding.numbers.text = addToInputText("÷")
-        }
-        binding.Multiplication.setOnClickListener {
-            binding.numbers.text = addToInputText("×")
-        }
-        binding.Minus.setOnClickListener {
-            binding.numbers.text = addToInputText("-")
-        }
-        binding.plus.setOnClickListener {
-            binding.numbers.text = addToInputText("+")
-        }
-        binding.clear.setOnClickListener {
-            clearInput()
-        }
-    }
-
-    private fun clearInput() {
-        binding.numbers.text = ""
-    }
-
-    fun onClickNumber(v: View){
-        val newDigit = (v as Button).text.toString()
-        val oldDigit = binding.numbers.text.toString()
-        val newTextNumber = oldDigit + newDigit
-        binding.numbers.text = newTextNumber
-    }
-
-    private fun addToInputText(buttonValue: String): String {
-        return "${binding.numbers.text}$buttonValue"
-    }
-
-    private fun getInputExpression(): String {
-        var expression = binding.numbers.text.replace(Regex("÷"), "/")
-        expression = expression.replace(Regex("×"), "*")
-        return expression
-    }
-
-    private fun showResult() {
-        val expression = getInputExpression()
-        val result = Expression(expression).calculate()
+    private fun Result(operation : String) {
+        val result = Expression(operation).calculate()
         if (result.isNaN()) {
             binding.operation.text = "Error"
         } else {
             binding.operation.text = DecimalFormat("0.######").format(result).toString()
         }
     }
-
     fun RX(){
         val observable = Observable.create<String>{ emitter ->
             binding.numbers.doOnTextChanged { text, start, before, count ->
@@ -82,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         }.debounce(1, TimeUnit.SECONDS)
         observable.subscribe(
             { t ->
-                showResult()
+                Result(t.toString())
             },
             { e ->
                 Log.i("TAG", "ON error")
